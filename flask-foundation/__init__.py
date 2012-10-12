62583b5481d530283f154445412bf60342743562
Zak Zajac <zak@madzak.com>
2012-10-12 16:53:58 -0700

    Stubbed out foundation blueprint class, static files, and templates

---
+++ flask-foundation/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from flask import Blueprint, current_app, url_for
+
+try:
+    from wtforms.fields import HiddenField
+except ImportError:
+    def is_hidden_field_filter(field):
+        raise RuntimeError('WTForms is not installed.')
+else:
+    def is_hidden_field_filter(field):
+        return isinstance(field, HiddenField)
+
+class Foundation(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config.setdefault('FOUNDATION_USE_MINIFIED', True)
+        app.config.setdefault('FOUNDATION_USE_CDN', False)
+
+        self.app = app
+        self.blueprint = Blueprint(
+            'foundation',
+            __name__,
+            template_folder='templates',
+            static_folder='static',
+            static_url_path=self.app.static_url_path + '/foundation')
+
+        app.register_blueprint(self.blueprint)
+
+        app.jinja_env.filters['foundation_is_hidden_field'] =\
+            is_hidden_field_filter
+        app.jinja_env.filters['foundation_find_resource'] =\
+            foundation_find_resource
